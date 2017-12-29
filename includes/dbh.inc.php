520cf8d479d827fa6325f516075e04c1361ad3e0
Robert <bobocionutrobert@gmail.com>
2017-12-29 21:04:54 +0200

    simple login/logout

---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServename = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "loginsystem";
+
+$conn = mysqli_connect($dbServename, $dbusername, $dbpassword, $dbname);
+?>
